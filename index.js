c3362c82213aa68ff986667a6b3b00a7be8ef2af
alfonso.lorenzana@gmail.com <alfonso.lorenzana@gmail.com>
2025-02-22 16:26:20 -0500

    index.js import

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from 'fs'
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
